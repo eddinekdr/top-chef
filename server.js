098e5a33681451adc321548c16290d477bb07b0a
Kheir-Eddine Kouider <kouider.eddine@gmail.com>
2018-03-03 11:03:11 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var app     = express();
 app.get('/scrape', function(req, res){
     for (var i = 1; i <= 1; i++) {
     	var restoetoile= [];
-    	var currentrestoetoile = {};
-        //michelin.restaurants(i);
-        currentrestoetoile.name = michelin.restaurants(i);
-        restoetoile.push(currentrestoetoile);
-        console.log(currentrestoetoile);
-        lafourchette.restaurants2(i,restoetoile);
+        restoetoile = michelin.restaurants(i);
+        //console.log(rest);
+      
+        for (var j; i<= j<= 3; j++){
+        	restoetoile[j];
+        	lafourchette.restaurants2(i,restoetoile);
+        }
 }   
 })
+
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
